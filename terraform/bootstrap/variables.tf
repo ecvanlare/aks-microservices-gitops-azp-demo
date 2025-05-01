937cb9f7f1edcf77f8f81ea026aa7dd538a14ba7
E.C <50402539+ecvanlare@users.noreply.github.com>
2025-05-01 22:55:41 +0100

    bootstrap tags

--- terraform/bootstrap/variables.tf
+++ terraform/bootstrap/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group_name" {
-  description = "The name of the bootstrap resource group"
+  description = "The name of the resource group for the Online Boutique project"
   type        = string
   default     = "rg-online-boutique-bootstrap"
 }
 
 variable "resource_group_location" {
-  description = "The Azure region where the bootstrap resources will be created"
+  description = "The Azure region where the resource group will be created"
   type        = string
   default     = "uksouth"  # London region
 }
@@ -20,4 +20,30 @@ variable "storage_container_name" {
   description = "The name of the storage container for Terraform state"
   type        = string
   default     = "online-boutique-bootstrap-tfstate"
+}
+
+variable "tags" {
+  description = "Common tags to be applied to all resources"
+  type        = map(string)
+  default = {
+    environment = "bootstrap"
+    project     = "online-boutique"
+    managed_by  = "terraform"
+  }
+}
+
+variable "resource_group_tags" {
+  description = "Additional tags specific to the resource group"
+  type        = map(string)
+  default = {
+    purpose = "infrastructure"
+  }
+}
+
+variable "storage_account_tags" {
+  description = "Additional tags specific to the storage account"
+  type        = map(string)
+  default = {
+    purpose = "terraform-state"
+  }
 } 
