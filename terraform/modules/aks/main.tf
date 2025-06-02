7c6f9dbb7d8fc291d7c38941a0c07cfa68f22085
E.C <50402539+ecvanlare@users.noreply.github.com>
2025-06-02 21:28:10 +0100

    aks ref

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -1,18 +1,18 @@
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.aks_name
+  name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
-  dns_prefix          = var.aks_dns_prefix
+  dns_prefix          = var.dns_prefix
 
   default_node_pool {
     name                = "default"
-    node_count          = var.aks_node_count
-    vm_size             = var.aks_vm_size
-    os_disk_size_gb     = var.aks_os_disk_size_gb
-    enable_auto_scaling = var.aks_enable_auto_scaling
-    min_count           = var.aks_min_count
-    max_count           = var.aks_max_count
-    vnet_subnet_id      = var.subnet_id
+    node_count          = var.node_pool.node_count
+    vm_size             = var.node_pool.vm_size
+    os_disk_size_gb     = var.node_pool.os_disk_size_gb
+    enable_auto_scaling = var.node_pool.enable_auto_scaling
+    min_count           = var.node_pool.min_count
+    max_count           = var.node_pool.max_count
+    vnet_subnet_id      = var.network.subnet_id
   }
 
   identity {
@@ -20,8 +20,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 
   network_profile {
-    network_plugin     = var.aks_network_plugin
-    network_policy     = var.aks_network_policy
+    network_plugin     = var.network.plugin
+    network_policy     = var.network.policy
     load_balancer_sku  = "standard"
   }
 
--- terraform/modules/aks/variables.tf
+++ terraform/modules/aks/variables.tf
@@ -8,67 +8,35 @@ variable "location" {
   type        = string
 }
 
-variable "aks_name" {
+variable "name" {
   description = "Name of the AKS cluster"
   type        = string
 }
 
-variable "aks_dns_prefix" {
+variable "dns_prefix" {
   description = "DNS prefix for the AKS cluster"
   type        = string
 }
 
-variable "aks_node_count" {
-  description = "Number of nodes in the default node pool"
-  type        = number
-  default     = 1
+variable "node_pool" {
+  description = "Node pool configuration"
+  type = object({
+    node_count         = number
+    vm_size            = string
+    os_disk_size_gb    = number
+    enable_auto_scaling = bool
+    min_count          = optional(number)
+    max_count          = optional(number)
+  })
 }
 
-variable "aks_vm_size" {
-  description = "Size of the VM for the default node pool"
-  type        = string
-  default     = "Standard_D2s_v3"
-}
-
-variable "aks_os_disk_size_gb" {
-  description = "OS disk size in GB for the default node pool"
-  type        = number
-  default     = 30
-}
-
-variable "aks_enable_auto_scaling" {
-  description = "Enable auto scaling for the default node pool"
-  type        = bool
-  default     = true
-}
-
-variable "aks_min_count" {
-  description = "Minimum number of nodes for auto scaling"
-  type        = number
-  default     = 1
-}
-
-variable "aks_max_count" {
-  description = "Maximum number of nodes for auto scaling"
-  type        = number
-  default     = 3
-}
-
-variable "aks_network_plugin" {
-  description = "Network plugin for the AKS cluster"
-  type        = string
-  default     = "azure"
-}
-
-variable "aks_network_policy" {
-  description = "Network policy for the AKS cluster"
-  type        = string
-  default     = "azure"
-}
-
-variable "subnet_id" {
-  description = "ID of the subnet where the AKS cluster will be deployed"
-  type        = string
+variable "network" {
+  description = "Network configuration"
+  type = object({
+    plugin    = string
+    policy    = string
+    subnet_id = string
+  })
 }
 
 variable "tags" {
