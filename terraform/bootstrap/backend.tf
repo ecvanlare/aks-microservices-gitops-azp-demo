10822a8c8f1004284a01bc04a2a27129e544c93a
E.C <50402539+ecvanlare@users.noreply.github.com>
2025-06-03 15:40:48 +0100

    backnd config

--- terraform/bootstrap/backend.tf
+++ terraform/bootstrap/backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-online-boutique-bootstrap"
-    storage_account_name = "stonlineboutiquebootstf"
-    container_name       = "tfstate"
-    key                  = "bootstrap.tfstate"
-  }
+  backend "azurerm" {}
 }
