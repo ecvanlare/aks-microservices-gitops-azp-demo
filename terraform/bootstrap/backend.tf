56917f2d0d8b59b36cb59aced18f1a0e9e0ac328
E.C <50402539+ecvanlare@users.noreply.github.com>
2025-06-03 15:20:30 +0100

    backend_config removed

--- terraform/bootstrap/backend.tf
+++ terraform/bootstrap/backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "rg-online-boutique-bootstrap"
+    storage_account_name = "stonlineboutiquebootstf"
+    container_name       = "tfstate"
+    key                  = "bootstrap.tfstate"
+  }
 }
