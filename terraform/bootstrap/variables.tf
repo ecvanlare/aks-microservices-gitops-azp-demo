d7744dd9176b4921ad1f8ce89737a87ef2faf43a
E.C <50402539+ecvanlare@users.noreply.github.com>
2025-06-07 13:27:00 +0100

    bootstrap

--- terraform/bootstrap/variables.tf
+++ terraform/bootstrap/variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "The name of the resource group for the Online Boutique infrastructure"
   type        = string
-  default     = "rg-online-boutique-infra"
+  default     = "rg-online-boutique-bootstrap"
 }
 
 variable "resource_group_location" {
@@ -13,7 +13,7 @@ variable "resource_group_location" {
 variable "storage_account_name" {
   description = "The name of the storage account used to store Terraform state"
   type        = string
-  default     = "stonlineboutiqueinfratf"
+  default     = "stonlineboutiquebootstf"
 }
 
 variable "storage_container_name" {
@@ -32,7 +32,7 @@ variable "tags" {
   description = "Common tags to be applied to all resources"
   type        = map(string)
   default = {
-    environment = "infrastructure"
+    environment = "bootstrap"
     project     = "online-boutique"
     managed_by  = "terraform"
   }
@@ -42,7 +42,7 @@ variable "resource_group_tags" {
   description = "Additional tags specific to the resource group"
   type        = map(string)
   default = {
-    purpose = "infrastructure"
+    purpose = "bootstrap"
   }
 }
 
