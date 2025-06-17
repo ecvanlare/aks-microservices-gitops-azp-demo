87a3de7c0bc66d2f2cd15d8d82b57d503c9accd7
E.C <50402539+ecvanlare@users.noreply.github.com>
2025-06-17 01:11:44 +0100

    output

--- terraform/modules/aks/outputs.tf
+++ terraform/modules/aks/outputs.tf
@@ -17,8 +17,6 @@ output "cluster_id" {
 output "cluster_identity" {
   description = "The cluster identity used by the AKS cluster"
   value = {
-    id           = azurerm_kubernetes_cluster.aks.identity[0].id
-    client_id    = azurerm_kubernetes_cluster.aks.identity[0].client_id
     principal_id = azurerm_kubernetes_cluster.aks.identity[0].principal_id
   }
 }
@@ -26,8 +24,6 @@ output "cluster_identity" {
 output "kubelet_identity" {
   description = "The kubelet identity used by the AKS cluster"
   value = {
-    id           = azurerm_kubernetes_cluster.aks.kubelet_identity[0].id
-    client_id    = azurerm_kubernetes_cluster.aks.kubelet_identity[0].client_id
     principal_id = azurerm_kubernetes_cluster.aks.kubelet_identity[0].principal_id
   }
 } 
